722763d08622f4c4f3c0c3c1b69ce9a168f2e20e
Vic Nic <110270917+vixvix90@users.noreply.github.com>
2022-09-12 13:25:17 +1200

    Vics Component

---
+++ src/components/post.vue
@@ -0,0 +1,35 @@
+<template>
+ <!-- create new post start -->
+ <div v-if="formClicked" id="updateInfo">
+        <h3>Create new post</h3>
+
+        <input type="text" placeholder="Title" v-model="formValues.title">
+        <input type="text" placeholder="ImageUrl" v-model="formValues.imageUrl">
+        <input type="text" placeholder="Location" v-model="formValues.location">
+        <div class="formBtnFlex">
+        <button @click="insertDoc" class="formBtn blue "> Post </button>
+        <button @click="formClicked = false" class="formBtn black"> Cancel </button>
+        </div>
+
+      </div>   
+  <!-- end of create new post ection -->
+
+</template>
+
+<script >
+    export default{
+        props: {
+            editState: Boolean,
+            formClicked: Boolean,
+            formValues: Object
+        },
+        method: {
+        insertDoc(){
+            this.$emit("call-insertDoc")
+          
+        }
+    }
+    }
+
+   
+</script>
