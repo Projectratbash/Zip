3f99b1faf536db96497031bd673563694807cd8e
Eddie-Sc <scales.ed10@hotmail.co.nz>
2022-09-19 21:08:12 +1200

    Update login.vue

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,43 +1,71 @@
 <template>
-    <div v-if="!showMain" id="loginContainer">
+  <div v-if="!showMain" id="loginContainer">
 
     <div id="logoContainer">
-        <img src="src/assets/ZIPLogoCropped.png" alt="">
+      <img src="src/assets/ZIPLogoCropped.png" alt="">
     </div>
 
     <div v-if="!loginClicked" class="btnContainer" @click="loginClicked = true">
-        <button class="loginBtn blue">Log in</button>
-        <button class="loginBtn blue">Sign up</button>
+      <button class="loginBtn blue">Log in</button>
+      <button class="loginBtn blue">Sign up</button>
     </div>
     <div v-if="loginClicked" class="btnContainer">
-        <p id="loginText">Login</p>
-        <input class="loginInput" placeholder="Username" type="text">
-        <input class="loginInput" placeholder="Password" type="text">
-        <button class="loginBtn blue" @click="showMain = true, showEvent()">Log in</button>
-    </div>
+      <p id="loginText">Login</p>
+      <input class="loginInput" id= "namelogin" placeholder="Username" type="text" v-model="nameInput"  @input="getLength" >
+      <input class="loginInput" id="passwordlogin" placeholder="Password" type="text" v-model="myInput" @input="getLength" >
+  
 
-    </div>
+    
+<span>{{testVariable}}</span>
+
+      <button  class="loginValid"  :disabled="!myInput.length || !nameInput.length"  @click="showMain = true, showEvent()" >Log in</button> 
+
+
+  </div>
+</div>
 </template>
 
 <script>
-    export default {
-
-        props: {
-
-            loginClicked: Boolean,
-            showMain: Boolean
-
-        },
-        methods: {
-            showEvent(){
-            this.$emit('showEvent')
-        }
-    }
-}
+export default {
+data(){
+  return{
+nameInput: '',
+    myInput:'',
+  }
+},
+ 
+  props: {
+    loginClicked: Boolean,  
+    showMain: Boolean,
+  },
+  methods: {
+    showEvent() {
+      this.$emit('showEvent')
+    },
+    getLength: function () {
+      this.length = this.myInput.length
+     
+    },
+  
+}} 
 </script>
 
 <style>
-    #loginContainer {
+.loginValid:disabled {
+  background: none;
+  color: rgba(25, 134, 163, 1);
+  opacity: 0%;
+  cursor: not-allowed;
+}
+.loginValid{
+  background-color: rgba(25, 134, 163, 1);
+  border: none;
+  padding: 8px 50px 8px 50px;
+  border-radius: 5px;
+  margin-bottom: 15px;
+color: #fff;
+}
+#loginContainer {
   width: 100vw;
   height: 100vh;
   display: flex;
@@ -48,12 +76,10 @@
   background-size: cover;
   background-position-y: bottom;
 }
-
 #loginContainer img {
   width: 70%;
   margin: 0 10px 27px 0;
 }
-
 #logoContainer {
   display: flex;
   justify-content: center;
@@ -64,19 +90,16 @@
   border-radius: 50%;
   margin: 10vh 0 10vh 0;
 }
-
 #loginText {
   color: white;
   font-weight: 1000;
 }
-
 .btnContainer {
   display: flex;
   flex-direction: column;
   ;
   align-items: center;
 }
-
 .loginBtn {
   color: white;
   border: none;
@@ -84,7 +107,6 @@
   border-radius: 5px;
   margin-bottom: 15px;
 }
-
 .loginInput {
   border: rgba(25, 134, 163, 1) solid 2px;
   border-radius: 5px;
@@ -93,6 +115,3 @@
   margin-bottom: 20px;
 }
 </style>
-
-// <!-- The login component is currently in App.vue -->
-// <!-- This component is not yet linked to App.vue -->
